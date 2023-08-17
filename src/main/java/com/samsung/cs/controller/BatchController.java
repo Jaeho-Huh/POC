1d909147c3620959f16f5b1e3e6d3437b70583aa
jinsubEom <jinsub8682@gmail.com>
2023-08-17 14:29:04 +0900

    중복 실행 방지 로직 추가

--- src/main/java/com/samsung/cs/controller/BatchController.java
+++ src/main/java/com/samsung/cs/controller/BatchController.java
@@ -5,15 +5,18 @@ import java.time.ZoneId;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.ui.Model;
@@ -24,6 +27,7 @@ public class BatchController {
 	
 	@Autowired 
 	JobLauncher jobLauncher;
+
 	
 	 @Value("#{T(java.time.LocalDate).now()}")
 	 private LocalDate currentDate;
@@ -31,16 +35,25 @@ public class BatchController {
 	@Autowired 
 	@Qualifier("createJob") Job job;
 	
+	private boolean isJobRunning = false; 
+	
     @GetMapping("/run-batch")
+    @Async
     public String runBatch(Model model) {
+    	
+    	if(isJobRunning) {
+    		model.addAttribute("message","Batch Job is already running");
+    		return "run-batch";
+    	}
+    	
     	JobParametersBuilder builder = new JobParametersBuilder();
         java.util.Date currentDateAsDate = Date.from(currentDate.atStartOfDay(ZoneId.systemDefault()).toInstant()); // Convert LocalDate to java.util.Date
+        
         builder.addLong("timestamp", System.currentTimeMillis()); 
-        builder.addLong("step", 1L); 
-        //builder.addDate("currentDate", currentDateAsDate);
-    	//builder.addDate("currentDate", Date.valueOf(currentDate)); 
+        builder.addLong("step", 1L);  
         try {
         	
+        	isJobRunning = true; 
             jobLauncher.run(job, builder.toJobParameters() );
             model.addAttribute("message", "Batch job executed successfully!");
             
@@ -49,6 +62,8 @@ public class BatchController {
         	
             e.printStackTrace();
             model.addAttribute("message", "Error occurred while running batch job.");
+        }finally {
+        	isJobRunning = false; 
         }
         return "run-batch";
     }
